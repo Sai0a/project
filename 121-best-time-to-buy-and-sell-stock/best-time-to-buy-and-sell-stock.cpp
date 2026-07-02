4a0da80debda6fa8d4cac4bcf16ca4dbabee48d9
Sai0a <hjjyy2829@gmail.com>
2026-07-02 14:00:29 +0530

    Time: 0 ms (100.00%) | Memory: 97.5 MB (24.83%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int l=0;
+        int r=1;
+        int ans=0;
+        while(r<n){
+            if(prices[r]>prices[l]){
+                ans=max(ans,prices[r]-prices[l]);
+            }
+            else{
+                l=r;
+            }
+            r++;
+        }
+        return ans;
+    }
+};
