bcf78c80364b43e6a0ab4bd131cd86c3e8907903
Sai0a <hjjyy2829@gmail.com>
2026-07-10 02:23:31 +0530

    Time: 11 ms (11.86%) | Memory: 23.9 MB (30.86%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& arr) {
+        sort(arr.begin(),arr.end());
+        int n=arr.size();
+        vector<vector<int>>ans;
+        for(int i=0;i<n;i++){
+            int start=arr[i][0];
+            int end=arr[i][1];
+            if(!ans.empty() && ans.back()[1]>=end){
+                continue;
+            }
+            for(int j=i+1;j<n;j++){
+                if(arr[j][0]<=end){
+                    end=max(end,arr[j][1]);
+                }
+
+                else{
+                    break;
+                }
+            }
+            ans.push_back({start,end});
+        }
+        return ans;
+    }
+};
