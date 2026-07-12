0a1d9f84df236303c40e31615d7b922583cf758a
Sai0a <hjjyy2829@gmail.com>
2026-07-13 05:17:03 +0530

    Time: 27 ms (42.27%) | Memory: 139.1 MB (68.28%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -5,22 +5,18 @@ public:
         int n=nums.size();
         vector<int>ans;
         for(int i=0;i<n;i++){
-            //if overflow
             if(!q.empty() && q.front()==i-k){
                 q.pop_front();
             }
+
             while(!q.empty() && nums[q.back()] < nums[i]){
                 q.pop_back();
-
-
             }
             q.push_back(i);
 
-            
             if(i>=k-1){
                 ans.push_back(nums[q.front()]);
             }
-
         }
         return ans;
     }
