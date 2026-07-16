afff4001ca67079f25f5eb7e9edd39421e913c15
Sai0a <hjjyy2829@gmail.com>
2026-07-16 23:34:17 +0530

    Time: 4 ms (10.81%) | Memory: 71.8 MB (14.60%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -4,10 +4,10 @@ public:
         int n=nums.size();
         int currentsum=nums[0];
         int maxsum=nums[0];
-
         for(int i=1;i<n;i++){
             currentsum=max(nums[i],currentsum+nums[i]);
             maxsum=max(maxsum,currentsum);
+
         }
         return maxsum;
     }
