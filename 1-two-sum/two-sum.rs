abb60aecbc013fa2d2d8855916f0738e47743c7c
Sai0a <hjjyy2829@gmail.com>
2026-08-19 23:37:36 +0530

    Time: 0 ms (100.00%) | Memory: 2.5 MB (19.01%) - LeetSync

--- 1-two-sum/two-sum.rs
+++ 1-two-sum/two-sum.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut map = HashMap::new();
+        let mut map = HashMap::new(); // <-- fixed capitalization
         for (i, &num) in nums.iter().enumerate() {
             if let Some(&j) = map.get(&(target - num)) {
                 return vec![j as i32, i as i32];
