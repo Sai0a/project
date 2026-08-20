b21e53cc948570443ed6d58bd155bac9acf95b77
Sai0a <hjjyy2829@gmail.com>
2026-08-20 14:56:19 +0530

    Time: 0 ms (100.00%) | Memory: 2.2 MB (81.93%) - LeetSync

---
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn first_uniq_char(s: String) -> i32 {
+        let mut freq=vec![0;26];
+
+        for ch in s.chars() {
+            freq[(ch as u8 - b'a') as usize]+=1;
+        }
+        for (i,ch) in s.chars().enumerate() {
+            if freq[(ch as u8 - b'a') as usize]==1 {
+                return i as i32;
+
+            }
+        }
+        -1
+    }
+}
